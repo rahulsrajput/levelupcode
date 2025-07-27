815c2b894d22749157fab3b560f5d38c56733a52
rahulsrajput <rahulsrajput29@gmail.com>
2025-07-27 17:48:24 +0530

    feat(auth) : Login serializer and view , JWT installation & setup , Usersession model
    1- user session model for tracking login ip and userAgent
    2- JWT for statless authentication and refresh , access token
    3- login serializer , view and url.

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from account.views.register import registerView
 from account.views.verifyEmail import verifyEmail
+from account.views.login import loginView
 
 urlpatterns = [
     path('register/', registerView.as_view(), name='register'),
     path('verify-email/', verifyEmail.as_view(), name='verify-email'),
+    path('login/', loginView.as_view(), name='login'),
 ]
