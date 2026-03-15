532f172ebd88dfe9ca54d1ba89bdb9dc8f8eefaa
rahulsrajput <rahulsrajput29@gmail.com>
2026-03-15 13:52:28 +0000

    chore: working gmail smtp updated

--- backend/account/utils/gmailEmail.py
+++ backend/account/utils/gmailEmail.py
@@ -9,18 +9,19 @@ class SMTPResponse:
         self.text = text
 
 def send_real_email(to_email, user_name, link):
+    msg = EmailMessage()
+    msg['Subject'] = 'Verify your email'
+    msg['From'] = config('GMAIL_USERNAME')
+    msg['To'] = to_email
+    msg.set_content(f"Hi {user_name}, click here: {link}")
+    
     try:
-        msg = EmailMessage()
-        msg['Subject'] = 'Welcome to LevelUpCode'
-        msg['From'] = config('GMAIL_USERNAME')
-        msg['To'] = to_email
-        msg.set_content(f'Hi {user_name}, click here: {link}')
-        
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.starttls()
             smtp.login(config('GMAIL_USERNAME'), config('GMAIL_APP_PASSWORD'))
             smtp.send_message(msg)
-
-        return SMTPResponse(status_code=200, text="Email sent successfully")
+        print("Email sent successfully")
+        return SMTPResponse()
     except Exception as e:
-        return SMTPResponse(status_code=500, text=str(e))
+        print("Error sending email:", e)
+        raise e
