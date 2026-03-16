d2cb5fb9cb14c026261aa275ef0bcfe561185114
rahulsrajput <rahulsrajput29@gmail.com>
2026-03-16 10:04:36 +0000

    .

--- backend/account/utils/email.py
+++ backend/account/utils/email.py
@@ -1,40 +1,40 @@
-# import requests
-# from decouple import config
+import requests
+from decouple import config
 
-# MAILTRAP_API_URL = "https://sandbox.api.mailtrap.io/api/send/4011941"
+MAILTRAP_API_URL = "https://sandbox.api.mailtrap.io/api/send/4011941"
 
 
-# def send_mailtrap_mail(to_email, user_name, link, template_id):
-#     print(link)
-#     headers = {
-#         'Authorization' : f"Bearer {config('MAILTRAP_API_KEY')}",
-#         'Content-Type' : 'application/json'
-#     }
+def send_mailtrap_mail(to_email, user_name, link, template_id):
+    print(link)
+    headers = {
+        'Authorization' : f"Bearer {config('MAILTRAP_API_KEY')}",
+        'Content-Type' : 'application/json'
+    }
 
-#     payload = {
-#         "from" : {
-#             "email": config('DEFAULT_FROM_EMAIL'),
-#             "name": "LevelUpCode"
-#         },
+    payload = {
+        "from" : {
+            "email": config('DEFAULT_FROM_EMAIL'),
+            "name": "LevelUpCode"
+        },
         
-#         "to": [
-#             {
-#                 "email": to_email,
-#                 "name": user_name
-#             }
-#         ],
-#         "template_uuid": template_id,
-#         "template_variables": {
-#             "user_name": user_name,
-#             "link": link
-#         }
-#     }
+        "to": [
+            {
+                "email": to_email,
+                "name": user_name
+            }
+        ],
+        "template_uuid": template_id,
+        "template_variables": {
+            "user_name": user_name,
+            "link": link
+        }
+    }
 
-#     response = requests.post(
-#         MAILTRAP_API_URL,
-#         headers=headers,
-#         json=payload
-#     )
+    response = requests.post(
+        MAILTRAP_API_URL,
+        headers=headers,
+        json=payload
+    )
 
-#     # return response object for debugging
-#     return response
+    # return response object for debugging
+    return response
